9e54e7faa4caa26eadc37cb63da4353675fd7f81
marowak <radiant42@yandex.ru>
2023-02-24 22:45:01 +0200

    added java module

---
+++ java_spring_hw/src/main/java/com/example/java_spring_hw/JavaSpringHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringHwApplication.class, args);
+    }
+
+}
